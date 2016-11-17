57b21c4f99577cc0288ec1976202dd3e4f0101c4
HarryWu00 <8hwu@stpatrick.org>
2016-11-17 10:53:08 -0600

    add class function

---
+++ pizza.js
@@ -0,0 +1,42 @@
+class pizza{
+  //type constructor below
+  constructor(size,meatTopping,veggieTopping){
+    this.size=s;
+    this.meatTopping=mT;
+    this.veggieTopping=vT;
+  }
+
+
+  //type instance functions below
+
+  sizeCost(){
+  if(this.size=small){
+    sizeCost=7.99;
+  }
+  else if(this.size=medium){
+    sizeCost=9.99;
+  }
+  else if(this.size=large){
+    sizeCost=12.99
+  }
+  else{
+    sizeCost=15.99;
+  }
+  return sizeCost
+}
+
+  toppingCost(top){
+    if(this.top=meatTopping){
+      
+    }
+
+  }
+
+  price(){
+
+  }
+
+  //type class functions below
+
+
+}
